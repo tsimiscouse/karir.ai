ef9804013205bf8dd9b7e64663508cb1513de435
tsimiscouse <attaqi21@gmail.com>
2025-04-23 14:51:33 +0700

    Feat: Remove redundant children wrapper from RootLayout for cleaner structure

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -27,7 +27,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
         <main>{children}</main>
         <div id="portal-root" />
       </body>
